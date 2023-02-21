9811250ba897619814ff89a305c136c9cf067baf
JONATHAN WAUNG <u1119325@umail.utah.edu>
2023-02-20 22:28:45 -0700

    Finished

--- app/src/main/java/com/example/app1/LoginActivity.kt
+++ app/src/main/java/com/example/app1/LoginActivity.kt
@@ -1,5 +1,24 @@
 package com.example.app1
 
-class LoginActivity {
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
+class LoginActivity : AppCompatActivity(){
+    var textFullName: TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        textFullName = findViewById(R.id.login_confirmed)
+
+        val intentFromMain = intent
+
+        var first = intentFromMain.getStringExtra("FIRST_NAME")
+        var last = intentFromMain.getStringExtra("LAST_NAME")
+
+        textFullName!!.text = first + " " + last + " is logged in!"
+    }
 }
--- app/src/main/java/com/example/app1/MainActivity.kt
+++ app/src/main/java/com/example/app1/MainActivity.kt
@@ -3,20 +3,28 @@ package com.example.app1
 import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.os.PersistableBundle
 import android.provider.MediaStore
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import java.io.File
+import java.io.FileOutputStream
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     private var firstName: String? = null
     private var middleName: String? = null
     private var lastName: String? = null
+    private var imagePath: String? = null
 
     private var firstEdit: EditText? = null
     private var middleEdit: EditText? = null
@@ -24,6 +32,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private var cameraButton: Button? = null
     private var profilePicture: ImageView? = null
+    private var isPicTaken = false
 
     private var submitButton: Button? = null
 
@@ -47,7 +56,41 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         loginIntent = Intent(this, LoginActivity::class.java)
 
         if (savedInstanceState != null) {
-            //names and picture
+            val fn = savedInstanceState.getString("FIRST_NAME")
+            if (fn != null)
+                firstName = fn
+            val mn = savedInstanceState.getString("MIDDLE_NAME")
+            if (mn != null)
+                middleName = mn
+            val ln = savedInstanceState.getString("LAST_NAME")
+            if (ln != null)
+                lastName = ln
+            val imagePath = savedInstanceState.getString("IMAGE_PATH")
+            if (imagePath != null) {
+                val image = BitmapFactory.decodeFile(imagePath)
+                if (image != null)
+                    profilePicture!!.setImageBitmap(image)
+            }
+        }
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val fn = savedInstanceState.getString("FIRST_NAME")
+        if (fn != null)
+            firstName = fn
+        val mn = savedInstanceState.getString("MIDDLE_NAME")
+        if (mn != null)
+            middleName = mn
+        val ln = savedInstanceState.getString("LAST_NAME")
+        if (ln != null)
+            lastName = ln
+        val path = savedInstanceState.getString("IMAGE_PATH")
+        if (path != null) {
+            imagePath = path
+            val image = BitmapFactory.decodeFile(path)
+            if (image != null)
+                profilePicture!!.setImageBitmap(image)
         }
     }
 
@@ -62,14 +105,26 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                }
             }
             R.id.button_submit -> {
-                //save names and start new activity
+                firstName = firstEdit!!.text.toString()
+                middleName = middleEdit!!.text.toString()
+                lastName = lastEdit!!.text.toString()
+                if (firstName == "" || middleName == "" || lastName =="" || !isPicTaken) {
+                    Toast.makeText(this@MainActivity, "All fields are required", Toast.LENGTH_SHORT).show()
+                    return
+                }
+                loginIntent!!.putExtra("FIRST_NAME", firstName)
+                loginIntent!!.putExtra("LAST_NAME", lastName)
+                startActivity(loginIntent)
             }
         }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-
+        outState.putString("FIRST_NAME", firstName)
+        outState.putString("MIDDLE_NAME", middleName)
+        outState.putString("LAST_NAME", lastName)
+        outState.putString("IMAGE_PATH", imagePath)
     }
     private val openCamera = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result ->
         if (result.resultCode == RESULT_OK) {
@@ -77,6 +132,32 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             val image = result.data!!.getParcelableExtra("data", Bitmap::class.java)
             profilePicture!!.setImageBitmap(image)
             cameraButton!!.text = "Retake?"
+            if (Environment.MEDIA_MOUNTED == Environment.getExternalStorageState()) {
+                val path = saveImage(image)
+                imagePath = path
+            }
+            isPicTaken = true
         }
+    }
+
+    private fun saveImage(imageBitmap: Bitmap?): String {
+        val dirRoot = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val localDir = File("$dirRoot/saved_images")
+        localDir.mkdirs()
+        val time = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val name = "Image_$time.jpg"
+        val file = File(localDir, name)
+        if (file.exists())
+            file.delete()
+        try {
+            val output = FileOutputStream(file)
+            imageBitmap!!.compress(Bitmap.CompressFormat.JPEG, 90, output)
+            output.flush()
+            output.close()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return file.absolutePath
+
     }
 }
